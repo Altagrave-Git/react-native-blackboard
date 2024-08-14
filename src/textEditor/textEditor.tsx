36e2bae21d8b01d1dd256ca6d75f18c478b061d7
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-08-14 01:53:49 -0600

    feat: update TextEditor web view to use react dev server

--- src/textEditor/textEditor.tsx
+++ src/textEditor/textEditor.tsx
@@ -12,34 +12,12 @@ const TextEditor = ({ data, setData }: TextEditorProps) => {
 
   return (
     <WebView
-      source={{
-        html: `
-          <!DOCTYPE html>
-          <html>
-            <head>
-              <style>
-                body { font-size: 20px }
-              </style>
-            </head>
-            <body style="background-color: black; color: white;">
-              <h1>Text Editor</h1>
-              <p contenteditable>Write something here...</p>
-              <script>
-                window.onload = (event) => {
-                  if (window.ReactNativeWebView.injectedObjectJson()) {
-                    const data = JSON.parse(window.ReactNativeWebView.injectedObjectJson()).data;
-                    document.querySelector('p').innerText = data;
-                  }
-                }
-              </script>
-            </body>
-          </html>
-        `,
-      }}
+      source={{ uri: 'http://10.0.0.222:3000' }}
       scalesPageToFit={false}
       useWebView2={true}
       javaScriptEnabled={true}
       injectedJavaScriptObject={{ data: 'This is useful data.' }}
+      originWhitelist={['*']}
       style={styles.container}
     />
   );
